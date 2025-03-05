01e6c1c10c63efd8a9120b1297d988924cc6b00b
kewuaa <202321011111@std.uestc.edu.cn>
2025-03-05 13:11:42 +0800

    first commit

---
+++ src/buffer.cpp
@@ -0,0 +1,114 @@
+#include <utility>
+#include <cassert>
+
+#include <spdlog/spdlog.h>
+
+#include "buffer.hpp"
+
+
+GrowableBuffer::View::View(GrowableBuffer& buf, size_t begin, size_t size) noexcept:
+    _buf(buf),
+    _begin(begin),
+    _size(size)
+{
+    //
+}
+
+
+void GrowableBuffer::_swap_to_begin() noexcept {
+    auto nbytes = readable_bytes();
+    std::memcpy(
+        _buffer.data(),
+        _buffer.data() + _read_idx,
+        nbytes
+    );
+    _read_idx = 0;
+    _write_idx = nbytes;
+}
+
+
+void GrowableBuffer::_make_available(size_t nbytes) noexcept {
+    auto diff = nbytes - writable_bytes();
+    if (diff <= 0) {
+        return;
+    }
+    if (diff <= _prependable_bytes()) {
+        _swap_to_begin();
+        return;
+    }
+    _buffer.resize(_buffer.size() + (diff < 15 ? 15 : diff));
+}
+
+
+GrowableBuffer::GrowableBuffer(size_t buffer_size) noexcept:
+    _buffer(buffer_size, 0)
+{
+    //
+}
+
+
+GrowableBuffer::GrowableBuffer(GrowableBuffer&& buffer) noexcept:
+    _buffer(std::move(buffer._buffer)),
+    _read_idx(std::exchange(buffer._read_idx, 0)),
+    _write_idx(std::exchange(buffer._write_idx, 0))
+{
+    //
+}
+
+
+GrowableBuffer& GrowableBuffer::operator=(GrowableBuffer&& buffer) noexcept {
+    _buffer = std::move(buffer._buffer);
+    _read_idx = std::exchange(buffer._read_idx, 0);
+    _write_idx = std::exchange(buffer._write_idx, 0);
+    return *this;
+}
+
+
+std::string_view GrowableBuffer::read(size_t nbytes) noexcept {
+    assert(readable_bytes() >= nbytes && "no enough data to load");
+    std::string_view res { _buffer.data() + _read_idx, nbytes };
+    _read_idx += nbytes;
+    if (_read_idx == _write_idx) {
+        _read_idx = _write_idx = 0;
+    }
+    return res;
+}
+
+
+std::string_view GrowableBuffer::read_all() noexcept {
+    std::string_view res { _buffer.data() + _read_idx, readable_bytes() };
+    _read_idx = _write_idx = 0;
+    return res;
+}
+
+
+GrowableBuffer::View GrowableBuffer::malloc(size_t nbytes) noexcept {
+    _make_available(nbytes);
+    View res = { *this, _write_idx, nbytes };
+    _write_idx += nbytes;
+    return res;
+}
+
+
+void GrowableBuffer::free(char* data) noexcept {
+    assert(_buffer.data() + _write_idx - data >= 0);
+    _write_idx = data - _buffer.data();
+}
+
+
+void GrowableBuffer::write(char byte) noexcept {
+    _make_available(1);
+    _buffer[_write_idx] = byte;
+    _write_idx++;
+}
+
+
+GrowableBuffer::View GrowableBuffer::write(std::string_view data) noexcept {
+    if (data.empty()) {
+        SPDLOG_WARN("try to write empty data");
+        return { *this, _write_idx, 0 };
+    }
+    auto view = this->malloc(data.size());
+    std::copy(data.begin(), data.end(), view.data());
+    return view;
+}
---
+++ src/buffer.hpp
@@ -0,0 +1,72 @@
+#pragma once
+#include <vector>
+#include <format>
+#include <cstring>
+#include <cstddef>
+
+
+
+class GrowableBuffer {
+    private:
+        std::vector<char> _buffer;
+        size_t _read_idx { 0 };
+        size_t _write_idx { 0 };
+
+        void _swap_to_begin() noexcept;
+        void _make_available(size_t n) noexcept;
+
+        inline size_t _prependable_bytes() const noexcept {
+            return _read_idx;
+        }
+    public:
+        class View {
+            friend GrowableBuffer;
+            private:
+                GrowableBuffer& _buf;
+                size_t _begin;
+                size_t _size;
+                View(GrowableBuffer& buf, size_t begin, size_t size) noexcept;
+            public:
+                View() = delete;
+                View& operator=(View&) = delete;
+                View& operator=(View&&) = delete;
+                View(View&) noexcept = default;
+                View(View&&) noexcept = default;
+                inline char* data() const noexcept {
+                    return _buf._buffer.data() + _begin;
+                }
+
+                inline size_t size() const noexcept {
+                    return _size;
+                }
+        };
+
+        GrowableBuffer(size_t buffer_size = DEFAULT_BUFFER_SIZE) noexcept;
+        GrowableBuffer(GrowableBuffer&) = default;
+        GrowableBuffer& operator=(GrowableBuffer&) = default;
+        GrowableBuffer(GrowableBuffer&&) noexcept;
+        GrowableBuffer& operator=(GrowableBuffer&& buffer) noexcept;
+        [[nodiscard]] std::string_view read(size_t nbytes) noexcept;
+        [[nodiscard]] std::string_view read_all() noexcept;
+        [[nodiscard]] View malloc(size_t nbytes) noexcept;
+        void free(char* data) noexcept;
+        void write(char byte) noexcept;
+        View write(std::string_view data) noexcept;
+
+        template<typename... Args>
+        View write(std::format_string<Args...> fmt, Args&&... args) noexcept {
+            return write(std::format(fmt, std::forward<Args>(args)...));
+        }
+
+        inline size_t readable_bytes() const noexcept {
+            return _write_idx - _read_idx;
+        }
+
+        inline size_t writable_bytes() const noexcept {
+            return _buffer.size() - _write_idx;
+        }
+
+        inline size_t size() const noexcept {
+            return _buffer.size();
+        }
+};
